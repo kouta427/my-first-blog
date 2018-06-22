370f03e99ef379e92ee414813d439a245cc1704f
Your Name <you@example.com>
2018-06-22 13:20:59 +0900

    renewal

--- pythontest/p9_4.py
+++ pythontest/p9_4.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
-steps = [6543, 7000, 8900, 10789, 3467, 11045, 5095]
-labels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+steps = [8000, 6000, 4354, 7654, 6532, 3424, 3245] 
+labels = ['C/C++', 'Python', 'Ruby', 'Java', 'PHP', 'C#', 'R']
 num_bars = len(steps)
 positions = range(1, num_bars+1)
 plt.bar(labels, steps, align='center')
 #plt.yticks(positions, labels)
-plt.ylabel('Steps')
-plt.xlabel('Day')
-plt.title('Number of steps walked')
+plt.ylabel('Num')
+plt.xlabel('Language')
+plt.title('Population of programming language')
 plt.grid()
 plt.show()

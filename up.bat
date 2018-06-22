92f5287f4c6b70f8b29892619c1e9d39f431add9
Your Name <you@example.com>
2018-06-22 13:31:20 +0900

    test

--- up.bat
+++ up.bat
@@ -1,5 +1,6 @@
 git add --all .
-git commit -m "renewal"
+set /P USR_INPUT_STR="•¶Žš—ñ‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢: "
+git commit -m %USR_INPUT_STR%
 git remote add origin https://github.com/kouta427/my-first-blog.git
 git push -u origin master
 
